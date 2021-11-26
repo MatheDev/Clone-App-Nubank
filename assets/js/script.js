e72ef610d9f6a7b01cccf5ef9dab51b542cd45c1
MatheDev <matheusdeveloperr@gmail.com>
2021-11-26 08:09:18 -0300

    completed

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function sob(){
+  if(document.getElementById('my-card-sob').style.display == 'none'){
+    document.getElementById('my-card-sob').style.display = 'block';
+    
+  }else{
+    document.getElementById('my-card-sob').style.display = 'none'
+  }
+}
